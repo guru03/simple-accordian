2c1c6f85e434e98414e80d40c69a5b115a4216a8
guru03 <guru03@outlook.com>
2017-12-01 19:24:57 +0530

    c

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $('.toggle').click(function(e) {
   var $this = $(this);
 
   if ($this.next().hasClass('show')) {
-      $this.next().removeClass('show');
-      $this.next().slideUp(350);
+      $this.next().removeClass('show').slideUp(350);
+  //    $this.next()
   } else {
       $this.parent().parent().find('li .inner').removeClass('show');
       $this.parent().parent().find('li .inner').slideUp(350);
